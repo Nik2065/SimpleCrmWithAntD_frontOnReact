66e417f6fb63d20f25f1094d8c605a13156ff5ec
Nik2065 <ivanov_nikolay@bk.ru>
2022-02-08 16:24:00 +0300

    editing main page

--- Src/simple-crm-app/src/_Components/Main.js
+++ Src/simple-crm-app/src/_Components/Main.js
@@ -4,8 +4,9 @@ import React from 'react';
 import 'antd/dist/antd.css';
 //import './index.css';
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -14,12 +15,25 @@ function Main() {
     return (
 <Layout>
     <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      {/*<div className="logo" />*/}
+        <Row>
+            <Col span={20}>
+                <span style={{color:"white"}}>Title</span>
+            </Col>
+            <Col span={4}>
+                <span style={{color:"white"}}>User login: someuser</span>
+                &nbsp;&nbsp;&nbsp;
+                <Button type="default" size='small'>Logout</Button>
+            </Col>
+        </Row>
+      {/*<Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          
+        {
         <Menu.Item key="1">nav 1</Menu.Item>
         <Menu.Item key="2">nav 2</Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
+        
+      </Menu>*/}
     </Header>
     <Layout>
       <Sider width={200} className="site-layout-background">
@@ -29,24 +43,15 @@ function Main() {
           defaultOpenKeys={['sub1']}
           style={{ height: '100%', borderRight: 0 }}
         >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+          <Menu.Item key="sub1" icon={<UserOutlined />}>Заказы</Menu.Item>
+          <Menu.Item key="sub2" icon={<LaptopOutlined />}>Клиенты</Menu.Item>
+          <SubMenu key="sub3" icon={<LaptopOutlined />} title="Настройки">
             <Menu.Item key="5">option5</Menu.Item>
             <Menu.Item key="6">option6</Menu.Item>
             <Menu.Item key="7">option7</Menu.Item>
             <Menu.Item key="8">option8</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
+
         </Menu>
       </Sider>
       <Layout style={{ padding: '0 24px 24px' }}>
